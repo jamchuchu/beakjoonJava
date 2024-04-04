69ab0b9f0f05959876fad8b3f5e7f33ca1d156d0
hyeminLapTop <gpals5987@gmail.com>
2024-04-05 03:43:56 +0900

    24.04.05

---
+++ src/main/java/Q1092A.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Q1092A {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        List<Integer> cranes = new ArrayList<>();
+        String[] str = reader.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            cranes.add(Integer.parseInt(str[i]));
+        }
+        Collections.sort(cranes, Collections.reverseOrder());
+
+
+
+
+        int m = Integer.parseInt(reader.readLine());
+        str = reader.readLine().split(" ");
+        List<Integer> boxes = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            boxes.add(Integer.parseInt(str[i]));
+        }
+        Collections.sort(boxes, Collections.reverseOrder());
+
+        if (boxes.get(0) > cranes.get(0)) {
+            System.out.println(-1);
+        } else {
+
+            int time = 0;
+            while (!boxes.isEmpty()) {
+                int crIdx = 0;
+                int bxIdx = 0;
+                while(crIdx < n){
+                    if(bxIdx == boxes.size()){
+                        break;
+                    }
+                    if(cranes.get(crIdx) >= boxes.get(bxIdx)){
+                        boxes.remove(bxIdx);
+                        crIdx++;
+                    }else{
+                        bxIdx++;
+                    }
+                }
+                time++;
+            }
+            System.out.println(time);
+        }
+    }
+}
---
+++ src/main/java/Q13975.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Q13975 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(reader.readLine());
+        for(int i = 0; i < t; i++) {
+            int n = Integer.parseInt(reader.readLine());
+            String[] str = reader.readLine().split(" ");
+            for(int j = 0; j < str.length; j++) {
+                
+            }
+        }
+    }
+}
